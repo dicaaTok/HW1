6bc57a0adf4f012cd733a785a85f4b89c06c095d
dicaaTok <dicaaGeek@gmail.com>
2025-01-08 16:40:46 +0600

    Homewoek

---
+++ src/HW1.java
@@ -0,0 +1,54 @@
+import com.sun.source.tree.CompilationUnitTree;
+
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+public class HW1 {
+    public static void main (String[]args) {
+        /*   //1
+        String m = "";
+        System.out.println(m);
+
+        //2
+        int NUM = -2;
+        System.out.println(NUM);
+
+        //3
+        String word;
+        word = String.valueOf(10);
+        System.out.println(word);
+
+        //4
+        System.out.println(5 + 10);
+        m = NUM + word;
+        System.out.println(m);
+
+        //5
+        System.out.println(NUM + word);
+        System.out.println(m + NUM + word);
+
+        //6
+        if (NUM < 0){
+            System.out.println("вы указали отрицателное число");
+        } else if (NUM > 0){
+            System.out.println("Вы указали положителное число");
+        } else if (NUM == 0){
+            System.out.println("Вы указали ноль");
+        }
+
+
+        //7
+        System.out.print("Здраствуйте ведите ваше имя: ");
+
+        //8
+        Scanner scanner = new Scanner(in);
+        String name = scanner.nextLine();
+
+        //9
+        System.out.println("Добро подаловат, " + name + "!");
+        scanner.close();*/
+
+
+        }
+    }
